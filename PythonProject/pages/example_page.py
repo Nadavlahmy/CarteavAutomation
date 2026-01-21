f0a1011f77e69eb8bfd13ea41857088fd7d43559
Nadav Lahmy <108174240+Nadavlahmy@users.noreply.github.com>
2026-01-21 18:10:26 +0200

    Update example_page.py

--- PythonProject/pages/example_page.py
+++ PythonProject/pages/example_page.py
@@ -1,5 +1,6 @@
 from playwright.sync_api import Page
 
+
 class ExamplePage:
     URL = "https://office.carteav.com"
 
@@ -10,14 +11,49 @@ class ExamplePage:
         self.page.goto(self.URL)
 
     def click_move_to_testing_user(self):
-        # Stable selector using button text
-        selector = 'button:has-text("Move to Testing User")'
-        # Wait until button is visible
+        selector = 'button:has-text("DEMO USER 1")'
         self.page.wait_for_selector(selector, state="visible", timeout=15000)
-        # Scroll into view in case it’s offscreen
         self.page.eval_on_selector(selector, "el => el.scrollIntoView()")
-        # Click the button
         self.page.click(selector)
+        print("➡ Clicked 'Move to Testing User'")
+
+    def open_poi_dropdown(self):
+        selector = ".topLeftActionsButtonsContainer > div:first-child"
+        self.page.wait_for_selector(selector, timeout=15000)
+        self.page.evaluate(
+            """(selector) => {
+                document.querySelector(selector).click();
+            }""",
+            selector
+        )
+        print("📂 Opened POI dropdown")
+
+    def click_move_to_spa_sababa(self):
+        selector = '#pois_items > div > div:nth-child(6) > div.poi_poiTextContainer__V4Q1q > div > div'
+        self.page.wait_for_selector(selector, state="visible", timeout=15000)
+        self.page.eval_on_selector(selector, "el => el.scrollIntoView()")
+        self.page.click(selector)
+        print("➡ Clicked 'Spa sababa'")
+
+    def click_order_vehicle(self):
+        # Click the "הזמן רכב" button by visible text
+        locator = self.page.locator('text=הזמן רכב')
+
+        locator.wait_for(state="visible", timeout=15000)
+        locator.scroll_into_view_if_needed()
+        locator.click(force=True)
+
+        print("➡ Clicked 'הזמן רכב'")
+
+    def click_start_driving(self):
+        # Click the "התחל נסיעה" button by visible text
+        locator = self.page.locator('text=התחל נסיעה')
+
+        locator.wait_for(state="visible", timeout=15000)
+        locator.scroll_into_view_if_needed()
+        locator.click(force=True)
+
+        print("➡ Clicked 'התחל נסיעה'")
 
     def title(self):
         return self.page.title()
